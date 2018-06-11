e23b370fcd0691abe586b2675d014dc0baa8b3ec
Taowy <921664773@qq.com>
2018-06-11 23:41:35 +0800

    com

---
+++ unitoto-front_end-feature/src/components/search-detail/mini-search-result.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul class="mini-search-result">
+    <p>User</p>
+    <br />
+    <li v-for="user in users" v-bind:key="user.id" class="user">
+      <Avatar v-bind:src=user.avatar />
+      {{user.name}}
+    </li>
+
+  </ul>
+</template>
+
+<style>
+  .mini-search-result {
+    display: block;
+    width: 50%;
+    margin:auto;
+    margin-top: 5px;
+    border: 0.1ex solid gray;
+  }
+
+  .user {
+    margin: 4px;
+  }
+</style>
+
+
+<script>
+  export default {
+    data: function () {
+      return {
+        users: [
+
+        ],
+        imgs: [
+
+        ]
+      }
+    }
+  }
+</script>
